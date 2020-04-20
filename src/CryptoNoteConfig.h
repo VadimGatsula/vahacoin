816ca6e2c022bd330b144fff63d74dc90f9ebe7e
Vadim Gatsula <mj.nooker@gmail.com>
2020-04-20 18:13:08 +0300

    Changed IPs of seednodes: added IP of seednode 1 for daemon

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "192.168.0.104:17236"
+  "18.184.222.248:17236"
   //"your_seed_ip2.com:8080",
 };
 
