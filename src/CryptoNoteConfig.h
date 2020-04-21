9b83b90b43f2ab12fd93a0ed5e37f8b02020121d
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-04-21 17:33:44 +0300

    Added GENESIS hash. Added vahacoin2 IP and local IPs for vahacoin1 and vahacoin2

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Vahacoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001c08f8af8ae5f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101d378d3700ba7cdab9ab3b18223cb3005481bf7303b8063302695ae79de49c163";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -106,7 +106,10 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "18.184.222.248:17236"
+  "18.184.222.248:17236",
+  "172.31.47.180:17236",
+  "18.156.121.67:17236",
+  "172.31.34.110:17236"
   //"your_seed_ip2.com:8080",
 };
 
